7ed94fa6220a20d1656dde17e26bfb1840cb2f33
alhasann <94994255+alhasann@users.noreply.github.com>
2022-01-11 21:33:38 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+x = Math.floor(Math.random() * 6);
+y = Math.floor(Math.random() * 6);
+var dice = ["dice1.png", "dice2.png", "dice3.png", "dice4.png", "dice5.png", "dice6.png"];
+// get a random element of the dice array  
+//dice1.png or anything else fetched from the dice array 
+var randomDice1 = dice[x];
+var randomDice2 = dice[y];
+// define an attriute for <img> 
+var attributer1 = "images/" + randomDice1;
+
+var attributer2 = "images/" + randomDice2;
+
+document.querySelector(".img1").setAttribute("src", attributer1);
+// set the attribute of imبللage of player two 
+document.querySelector(".img2").setAttribute("src", attributer2);
+
+// who is the winner 
+if (attributer1 > attributer2) {
+    x = "Player 1";
+} else if (attributer2 > attributer1) {
+    x = 'Player 2';
+} else {
+
+    document.querySelector('.winner').innerHTML = "DRAW";
+}
+document.getElementById('winner').innerHTML = x;
